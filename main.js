3e057cca8647c36774241628b6877fcf3475dbb5
Yeni <125455423+YeniUvwo@users.noreply.github.com>
2024-09-04 13:09:27 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,47 @@
 function redirectToAboutUs() {
-    window.location.href = './about.html';
+  window.location.href = './about.html';
 }
 
 function redirectToHomepage() {
   window.location.href = './index.html';
 }
+
+function toggleNav() {
+  const navCta = document.querySelector('.nav-cta-container');
+  const hamburgerIcon = document.querySelector('.hamburger-container');
+  const closeIcon = document.querySelector('.close-container');
+  
+  navCta.classList.toggle('active');
+  
+  if (navCta.classList.contains('active')) {
+    hamburgerIcon.style.display = 'none';
+    closeIcon.style.display = 'block';
+  } else {
+    hamburgerIcon.style.display = 'block';
+    closeIcon.style.display = 'none';
+  }
+}
+
+function handleButtonClick(event) {
+  event.preventDefault();
+  toggleNav();
+  document.querySelector('#appointment').scrollIntoView({ behavior: 'smooth' });
+}
+
+//form validation
+(function () {
+  'use strict'
+
+  var forms = document.querySelectorAll('.needs-validation')
+
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
